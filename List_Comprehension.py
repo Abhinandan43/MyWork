32d320f5a8bccbf419488e5125fe49132d82336b
Abhinandan kumar <44209512+Abhinandan43@users.noreply.github.com>
2020-06-12 21:38:47 +0530

    Add files via upload

---
+++ List_Comprehension.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 21:26:45 2020
+
+@author: Abhinandan
+"""
+x=int(input())
+y=int(input())
+z=int(input())
+n=int(input())
+print([[i,j,k] for i in range(0,x+1) for j in range(0,y+1) for k in range(0,z+1) if i+j+k !=n])
